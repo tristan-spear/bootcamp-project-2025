a954b6693c2b658169c7273c3f69686318d7b774
tristan-spear <tspear1704@gmail.com>
2025-12-12 00:05:00 -0800

    Remove old milestone folders from milestone-4 branch

--- bootcamp-milestone-3/src/components/footer.tsx
+++
@@ -1,9 +0,0 @@
- import style from "./navbar.module.css";
- 
- export default function Footer() {
-    return (
-        <footer className={style.gradient}>
-        © 2025 Tristan Spear | All Rights Reserved
-    </footer>
-    );
- }
